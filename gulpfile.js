374c72a77b2914d5b0ce00cad38334b1065108a6
tinlvv <levinhtin@live.com>
2015-08-25 23:12:25 +0700

    gulp inject done

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ var gulp = require('gulp'),
   sourcemaps = require('gulp-sourcemaps'),
   rev = require('gulp-rev'),
   del = require("del"),
+  clean = require('gulp-clean'),
+  jshint = require('gulp-jshint'),
   gutil = require('gulp-util'),
   using = require('gulp-using'),
   inject = require('gulp-inject');
@@ -22,58 +24,54 @@ var paths = {
 
 
 //-----------CLEAN--------------------
-gulp.task("clean:js", function(cb) {
-  // rimraf(paths.concatJsDest, cb);
-  return del([paths.webroot + 'dist/**/*.min.js'], cb);
+gulp.task('clean:js', function() {
+  // del([paths.webroot + 'dist/js/*.js'],cb);
+  // del([paths.webroot + 'dist/'],cb);
+  return gulp.src([paths.webroot + 'dist/js/*.js'])
+          .pipe(clean());
 });
-gulp.task("clean", ["clean:js"]);
+gulp.task('clean', ['clean:js']);
 
 
 //-----------MIN--------------------
-gulp.task('min:js', function(){
+gulp.task('min:js:app', function(){
   //min app
-  gulp.src(paths.appJs)
+  return gulp.src(paths.appJs)
     //.pipe(sourcemaps.init())
-    .pipe(concat({path: 'app.min.js', cwd: ''}))                     // Make a single file 
     .pipe(using())
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat({path: 'app.min.js', cwd: ''}))                     // Make a single file 
     .pipe(uglify())                                 // Make the file titchy tiny small
     .pipe(rev())                                    // Suffix a version number to it
     //.pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.webroot +'dist/js/'))    // Write single versioned file to build/release folder
     .on('error', gutil.log);
 
-    gulp.src('./bower.json')
-        .pipe(using())
-        // .pipe(sourcemaps.init())
-        .pipe(mainBowerFiles('**/*.js'))
-        .pipe(concat({path: 'vendor.min.js', cwd: ''}))                     // Make a single file 
-        .pipe(uglify())                                 // Make the file titchy tiny small
-        .pipe(rev())                                    // Suffix a version number to it
-        // .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.webroot +'dist/js/'))   // Write single versioned file to build/release folder
-        .on('error', gutil.log); 
 });
-
-gulp.task('min', ['clean', 'min:js']);
-
-gulp.task('build:dev', function () {
-  setTimeout(function(){
-    gulp.src('src/index.html')
-    //.pipe(inject(gulp.src(paths.webroot +'dist/app/*.min.js', {read: false}), {starttag: '<!-- inject:app:{{ext}} -->'}))
-    // .pipe(inject(gulp.src(paths.webroot +'dist/js/*.min.js', {read: false}), {starttag: '<!-- inject:vendor:{{ext}} -->'}))
-    .pipe(gulp.dest('.'));
-  }, 2000);
+gulp.task('min:js:vendor', function(){
+  return gulp.src('./bower.json')
+    // .pipe(sourcemaps.init())
+    .pipe(mainBowerFiles('**/*.js'))
+    .pipe(concat({path: 'vendor.min.js', cwd: ''}))                     // Make a single file 
+    .pipe(uglify())                                 // Make the file titchy tiny small
+    .pipe(rev())                                    // Suffix a version number to it
+    // .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.webroot +'dist/js/'))   // Write single versioned file to build/release folder
+    .on('error', gutil.log); 
 });
+gulp.task('min:js', ['clean:js', 'min:js:vendor', 'min:js:app']);
+gulp.task('min', ['min:js']);
 
-gulp.task('html:release', ['clean', 'min'], function () {
+gulp.task('html:release', ['min'], function () {
     gulp.src(paths.webroot + 'src/index.html')
     .pipe(using())
-    .pipe(inject(gulp.src(paths.webroot +'dist/app*.min.js', {read: false}), {starttag: '<!-- inject:app:{{ext}} -->'}))
-    .pipe(inject(gulp.src(paths.webroot +'dist/vendor*.min.js', {read: false}), {starttag: '<!-- inject:vendor:{{ext}} -->'}))
-    .pipe(gulp.dest(paths.webroot));
+    .pipe(inject(gulp.src(paths.webroot +'dist/**/app*.min.js', {read: false}), {starttag: '<!-- inject:app:{{ext}} -->'}))
+    .pipe(inject(gulp.src(paths.webroot +'dist/**/vendor*.min.js', {read: false}), {starttag: '<!-- inject:vendor:{{ext}} -->'}))
+    .pipe(gulp.dest(paths.webroot));  
 });
-
-gulp.task('connect', ['clean', 'min', 'html:release'], function() {
+//-----------------SERVER-----------------------------------
+gulp.task('connect', ['clean:js', 'min:js', 'html:release'], function() {
   connect.server({
     root: '.',
     livereload: true,
@@ -84,4 +82,4 @@ gulp.task('connect', ['clean', 'min', 'html:release'], function() {
 
 
 // Start the tasks
-gulp.task('default', ['clean', 'min', 'html:release', 'connect']);
+gulp.task('default', ['html:release', 'connect']);
