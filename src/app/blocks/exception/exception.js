cf98b3335a815c140af0f6880ec1d42ec8ae54a3
tinlvv <levinhtin@gmail.com>
2015-08-28 10:42:42 +0700

    injection exception.js

--- src/app/blocks/exception/exception.js
+++ src/app/blocks/exception/exception.js
@@ -6,6 +6,7 @@
         .factory('exception', exception);
 
     /* @ngInject */
+    exception.$inject = ['$q', 'logger'];
     function exception($q, logger) {
         var service = {
             catcher: catcher
